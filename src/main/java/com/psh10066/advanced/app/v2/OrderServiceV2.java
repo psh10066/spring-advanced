48ccc40297118bb2592caefd21f2d59b5637a40e
이호준 <psh10066@gmail.com>
2024-04-08 14:42:28 +0900

    로그 추적기 V2 - 적용

---
+++ src/main/java/com/psh10066/advanced/app/v2/OrderServiceV2.java
@@ -0,0 +1,28 @@
+package com.psh10066.advanced.app.v2;
+
+import com.psh10066.advanced.trace.TraceId;
+import com.psh10066.advanced.trace.TraceStatus;
+import com.psh10066.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
