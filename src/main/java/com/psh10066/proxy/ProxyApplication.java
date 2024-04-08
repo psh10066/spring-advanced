db690d697c94b746d287428bf452d27d2fe6ede7
이호준 <psh10066@gmail.com>
2024-04-08 22:46:36 +0900

    예제 프로젝트 만들기 v1

--- src/main/java/com/psh10066/proxy/ProxyApplication.java
+++ src/main/java/com/psh10066/proxy/ProxyApplication.java
@@ -1,9 +1,12 @@
 package com.psh10066.proxy;
 
+import com.psh10066.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "com.psh10066.proxy.app.v3") // 컴포넌트 스캔 피하기
 public class ProxyApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/psh10066/proxy/app/v1/OrderControllerV1.java
@@ -0,0 +1,15 @@
+package com.psh10066.proxy.app.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface OrderControllerV1 {
+
+    @GetMapping("/v1/request")
+    String request(@RequestParam String itemId);
+
+    @GetMapping("/v1/no-log")
+    String noLog();
+}
