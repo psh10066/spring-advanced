13644da760b8711346ac2dd0da46f5d79379de62
이호준 <psh10066@gmail.com>
2024-04-09 00:01:17 +0900

    프록시 패턴 - 예제 코드2

--- src/test/java/com/psh10066/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/com/psh10066/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package com.psh10066.proxy.pureproxy.proxy;
 
+import com.psh10066.proxy.pureproxy.proxy.code.CacheProxy;
 import com.psh10066.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import com.psh10066.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,16 @@ class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
