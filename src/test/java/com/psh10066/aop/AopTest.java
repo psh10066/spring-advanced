2fa12100c21f40cdcaf3f1b638014fe7fb67f29b
이호준 <psh10066@gmail.com>
2024-04-10 02:30:40 +0900

    스프링 AOP 구현5 - 어드바이스 순서

--- src/test/java/com/psh10066/aop/AopTest.java
+++ src/test/java/com/psh10066/aop/AopTest.java
@@ -2,7 +2,7 @@ package com.psh10066.aop;
 
 import com.psh10066.aop.order.OrderRepository;
 import com.psh10066.aop.order.OrderService;
-import com.psh10066.aop.order.aop.AspectV4Pointcut;
+import com.psh10066.aop.order.aop.AspectV5Order;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,7 +16,8 @@ import org.springframework.context.annotation.Import;
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
-@Import(AspectV4Pointcut.class)
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 class AopTest {
 
     @Autowired
