69832b351b08ea59f4ecd1568fcd9b0895d258eb
이호준 <psh10066@gmail.com>
2024-04-09 13:08:10 +0900

    JDK 동적 프록시 - 예제 코드

---
+++ src/test/java/com/psh10066/proxy/jdkdynamic/code/AInterface.java
@@ -0,0 +1,5 @@
+package com.psh10066.proxy.jdkdynamic.code;
+
+public interface AInterface {
+    String call();
+}
---
+++ src/test/java/com/psh10066/proxy/jdkdynamic/code/BInterface.java
@@ -0,0 +1,5 @@
+package com.psh10066.proxy.jdkdynamic.code;
+
+public interface BInterface {
+    String call();
+}
