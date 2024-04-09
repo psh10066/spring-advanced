84e3048718f1efa63e2c4a238f5495c00c2fd411
이호준 <psh10066@gmail.com>
2024-04-09 21:13:26 +0900

    프록시 팩토리 - 적용1

--- src/main/java/com/psh10066/proxy/ProxyApplication.java
+++ src/main/java/com/psh10066/proxy/ProxyApplication.java
@@ -2,7 +2,7 @@ package com.psh10066.proxy;
 
 import com.psh10066.advanced.trace.logtrace.LogTrace;
 import com.psh10066.advanced.trace.logtrace.ThreadLocalLogTrace;
-import com.psh10066.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import com.psh10066.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+@Import(ProxyFactoryConfigV1.class)
 @SpringBootApplication(scanBasePackages = "com.psh10066.proxy.app.v3") // 컴포넌트 스캔 피하기
 public class ProxyApplication {
 
