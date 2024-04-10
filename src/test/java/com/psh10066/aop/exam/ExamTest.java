25004718a0cdd4edb09fa861d05be0d32a68c8fd
이호준 <psh10066@gmail.com>
2024-04-10 21:01:48 +0900

    로그 출력 AOP

--- src/test/java/com/psh10066/aop/exam/ExamTest.java
+++ src/test/java/com/psh10066/aop/exam/ExamTest.java
@@ -1,11 +1,14 @@
 package com.psh10066.aop.exam;
 
+import com.psh10066.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
+@Import(TraceAspect.class)
 @SpringBootTest
 class ExamTest {
 
