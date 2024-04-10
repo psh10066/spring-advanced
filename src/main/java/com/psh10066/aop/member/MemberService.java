efef7b4711049e5c63fa9e579089914bbfebd64a
이호준 <psh10066@gmail.com>
2024-04-10 18:46:35 +0900

    [스프링 AOP - 포인트컷] 예제 만들기

---
+++ src/main/java/com/psh10066/aop/member/MemberService.java
@@ -0,0 +1,5 @@
+package com.psh10066.aop.member;
+
+public interface MemberService {
+    String hello(String param);
+}
---
+++ src/test/java/com/psh10066/aop/pointcut/ExecutionTest.java
@@ -0,0 +1,27 @@
+package com.psh10066.aop.pointcut;
+
+import com.psh10066.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+class ExecutionTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void printMethod() {
+        // public java.lang.String com.psh10066.aop.member.MemberServiceImpl.hello(java.lang.String)
+        log.info("helloMethod={}", helloMethod);
+    }
+}
